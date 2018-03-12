afcd28633f883ed98905b2654a630e6ad63e2c95
Mattia La Spina <mat_halo_95@hotmail.com>
2018-03-12 14:59:09 +0000

    ...

--- php/recetas_crear.php
+++ php/recetas_crear.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 $conn = new mysqli('localhost', 'root', '', 'cociname');
 $conn->query("SET NAMES 'utf8'");
